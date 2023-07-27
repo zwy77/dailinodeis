12f925bdecbb19ad67563d7af3d37320d5d7f39f
zwy77 <34792820+zwy77@users.noreply.github.com>
2023-07-27 20:52:13 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'e7579138-444e-450f-abcc-6d4f2eb2b341';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
